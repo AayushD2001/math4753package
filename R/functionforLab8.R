4d3a2b67975fae9dc6c325f9a05adf68d4dd94a3
Aayush Dalal <Aayush.A.Dalal-1@ou.edu>
2022-12-03 17:06:13 -0600

    Adding all functions and data

---
+++ R/functionforLab8.R
@@ -0,0 +1,16 @@
+#' Title
+#'
+#' @param n
+#' @param iter
+#'
+#' @return
+#' @export
+#'
+#' @examples
+myclt=function(n,iter){
+  y=runif(n*iter,0,5)
+  data=matrix(y,nr=n,nc=iter,byrow=TRUE)
+  sm=apply(data,2,sum)
+  hist(sm)
+  sm
+}
