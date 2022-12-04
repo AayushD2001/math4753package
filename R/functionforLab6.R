53737ff348a351fc6209ea298692fbdb5d85fe54
Aayush Dalal <Aayush.A.Dalal-1@ou.edu>
2022-12-03 18:00:14 -0600

    added functions

---
+++ R/functionforLab6.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param mu
+#' @param sigma
+#'
+#' @return
+#' @export
+#'
+#' @examples
+myncurve = function(mu, sigma){
+  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma))
+}
